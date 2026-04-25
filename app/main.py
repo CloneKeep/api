f7e55e49f94d177a2e3e47b0f195572385dd3b1e
LeeDS <ehdtn3540@gmail.com>
2026-04-26 08:15:14 +0900

    Docker 필요 모듈 및 api 한글 깨짐 수정

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get('/')
 def root():
-    return {'message': 'FastAPI 설치가 완료되었습니다!'}
+    content = {'message': 'FastAPI 설치가 완료되었습니다!'}
+    return JSONResponse(content=content, media_type="application/json; charset=utf-8")
+
