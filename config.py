0bb2febd5304416ebd0a54739fe6cfb751b040c0
LeeDS <ehdtn3540@gmail.com>
2026-05-03 12:26:09 +0900

    CORS, allow_origins 주소 분리하여 관리, 빠진 파일 추가

---
+++ config.py
@@ -0,0 +1,5 @@
+# 허용할 도메인 리스트
+ALLOWED_ORIGINS = [
+    "https://integrationschedulerproject.github.io/",  # GitHub Pages 주소
+    "http://localhost:8080",                           # 로컬 테스트용 (FastAPI)
+]
