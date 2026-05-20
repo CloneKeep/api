9e3dbb3d18b092f5f5727056c1676f84230a6519
yorusicu <jimi008285@gmail.com>
2026-05-20 21:51:36 +0900

    [database.py] 로컬테스트를 위한 env 설정

--- app/database.py
+++ app/database.py
@@ -3,8 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+# dotenv 모둘
+
+from  dotenv import load_dotenv
+
+load_dotenv()
+
 # 환경 변수 'DATABASE_URL' 사용 (GCP/GitHub Actions에서 주입)
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DB_URL 환경 변수 설정 안됨")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
